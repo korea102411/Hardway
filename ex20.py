03c96f1030486726958a00c95c00bded788d6693
korea102411 <korea102411@gmail.com>
2021-05-10 22:13:19 -0700

    #adding extra exercise

--- ex20.py
+++ ex20.py
@@ -30,6 +30,9 @@ print("Let's print three lines:")
 current_line = 1
 print_a_line(current_line, current_file)
 
+
+rewind(current_file)
+
 current_line = current_line + 1
 print_a_line(current_line, current_file)
 
---
+++ ex24.py
@@ -0,0 +1,24 @@
+print("Let's practice everything.")
+print('You\'d need to know \'bout escapes with \\ that do:')
+print('\n newlines and \t tabs.')
+
+poem = """
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+"""
+
+print("_____________")
+print(poem)
+print("_____________")
+
+
+five = 10 - 2 + 3 - 6
+print(f"This should be five: {five}")
+
+def secret_formula(started):
+
+
